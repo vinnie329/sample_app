1584fa9c7085c1e2d10bdc49606a0546783d9596
vinnie329 <vinnie329@gmail.com>
2013-07-29 19:17:40 +1000

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -1,8 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
-gem 'bootstrap-sass' , '2.1'
+gem 'bootstrap-sass' , '2.3.2.0'
 gem 'bcrypt-ruby', '3.0.1'
+gem 'turbolinks'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
-  before_save { |user| user.email = email.downcase }
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
   validates(:name, presence: true, length: { maximum: 50 } )
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -21,5 +22,19 @@ class User < ActiveRecord::Base
   
   validates(:password, presence: true, length: { minimum: 6 })
   validates(:password_confirmation, presence: true)
+  after_validation{ self.errors.messages.delete(:password_digest) }
 
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
